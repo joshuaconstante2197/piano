f568c6f7387fb63e51edae69619e9658fa1142fb
joshua constante <joshuaconstante2197@hotmail.com>
2020-06-09 20:58:00 -0400

    add color change for black keys

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   let keys=  [65,83,68,70,71,72,74,75,76,186,222]
-  let blacks=[81,87,69,82,84,89,85,73,79,80,219,221]
+  let blacks2=[81,87,89,85,219,221]
+  let blacks3=[69,82,84,73,79,80]
   let notes=[0,2,3,4,5,6,7,8,9]
   document.addEventListener('keydown', keyToNote)
   document.addEventListener('keyup',pressed)
   var key = document.querySelectorAll("#container .keys")
-  var black = document.querySelectorAll("#container .black2s")
-
+  var black2s = document.querySelectorAll("#container .black2s")
+  var black3s = document.querySelectorAll("#container .black3s")
+  
   function keyToNote(e){
     for(i=0;i<=keys.length;i++){
-      console.log(e.keyCode)
       if(e.keyCode === keys[i]){
         key[i+7].classList.add("pressed")
-      }else if(e.keyCode === blacks[i]){
-        black[i+5].classList.add("pressed")
+      }else if(e.keyCode === blacks2[i]){
+        black2s[i+2].style.backgroundColor="grey";
+      }else if(e.keyCode === blacks3[i]){
+        black3s[i+3].style.backgroundColor="grey";
+        
       }
     }
   }
@@ -22,8 +26,10 @@ document.addEventListener('DOMContentLoaded', () => {
     for(i=0;i<=keys.length;i++){
       if(e.keyCode === keys[i]){
         key[i+7].classList.remove("pressed")
-      }else if(e.keyCode === blacks[i]){
-        black[i+5].classList.remove("pressed")
+      }else if(e.keyCode === blacks2[i]){
+        black2s[i+2].style.backgroundColor="black";
+      }else if(e.keyCode === blacks3[i]){
+        black3s[i+3].style.backgroundColor="black";
       }
     }
   }
